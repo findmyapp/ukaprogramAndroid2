4be9dafdbc9e9e0bd31aadc9ea2a2c23182f3683
Ole Christian Roed <ocroed@gmail.com>
2011-07-12 15:37:29 +0200

    added option menu

--- src/no/uka/findmyapp/ukaprogram/activities/Client.java
+++ src/no/uka/findmyapp/ukaprogram/activities/Client.java
@@ -4,6 +4,9 @@ import no.uka.findmyapp.ukaprogram.R;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TabHost;
 
 public class Client extends TabActivity{
@@ -12,7 +15,7 @@ public class Client extends TabActivity{
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
+
 		TabHost tabHost = getTabHost();
 		TabHost.TabSpec spec;
 		Intent intent;
@@ -24,13 +27,62 @@ public class Client extends TabActivity{
 		intent = new Intent().setClass(this, FavoritesListActivity.class);
 		spec = tabHost.newTabSpec("favs").setIndicator("Favorites").setContent(intent);
 		tabHost.addTab(spec);
-		
+
 		intent = new Intent().setClass(this, CalendarActivity.class);
 		spec = tabHost.newTabSpec("cal").setIndicator("Calendar").setContent(intent);
 		tabHost.addTab(spec);
-		
-		tabHost.setCurrentTab(2);
-				
-	} // end onCreate()
 
+		intent = new Intent().setClass(this, FiveNextActivity.class);
+		spec = tabHost.newTabSpec("5next").setIndicator("De 5 neste").setContent(intent);
+		tabHost.addTab(spec);
+
+		tabHost.setCurrentTab(2);
+
+
+
+	} // end onCreate()
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();   
+		inflater.inflate(R.menu.main_menu, menu);   
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {  
+		// Handle item selection  
+		switch (item.getItemId()) {   
+		case R.id.calendar:  
+			openCalendar();   
+			return true;
+		case R.id.fiveNext:  
+			openFiveNext(); 
+			return true;
+		case R.id.fullList:  
+			openFullList(); 
+			return true;
+		default:  
+			return super.onOptionsItemSelected(item); 
+		}
+		
+	}
+	
+	public void openCalendar(){
+		Intent intent;
+		
+		intent = new Intent().setClass(this, CalendarActivity.class);
+		startActivity(intent);
+		
+	}
+	public void openFiveNext(){
+		Intent intent;
+		
+		intent = new Intent().setClass(this, FiveNextActivity.class);
+		startActivity(intent);
+	}
+	public void openFullList(){
+		Intent intent;
+		
+		intent = new Intent().setClass(this, EventListActivity.class);
+		startActivity(intent);
+	}
 }
---
+++ src/no/uka/findmyapp/ukaprogram/activities/FiveNextActivity.java
@@ -0,0 +1,94 @@
+package no.uka.findmyapp.ukaprogram.activities;
+
+import java.util.ArrayList;
+
+import no.uka.findmyapp.ukaprogram.R;
+import no.uka.findmyapp.ukaprogram.adapters.EventListAdapter;
+import no.uka.findmyapp.ukaprogram.models.Event;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
+
+
+public class FiveNextActivity extends Activity {
+	EventListAdapter eventAdapter;
+	ArrayList<Event> eventsArrayList;
+	ListView eventListView;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.five_next_list);
+
+		eventsArrayList = new ArrayList<Event>();
+		Log.i("re","TESTETSE");
+		eventListView = (ListView) findViewById(R.id.eventListView);
+		eventAdapter = new EventListAdapter(this, R.layout.list_adapter, eventsArrayList);
+		eventListView.setAdapter(eventAdapter);
+		////////
+		populateListView();
+	} // end onCreate()
+
+	public void onResume() {
+		super.onResume();
+	}	
+	private void populateListView() {
+		eventsArrayList.clear();
+
+		Event oktoberfest = new Event();
+		oktoberfest.setTitle("Oktoberfest");
+		oktoberfest.setPlace("Dødens dal");
+		oktoberfest.setEventType("Revy & Teater");
+		oktoberfest.setAgeLimit(18);
+		oktoberfest.setStartTime("17.00");
+		oktoberfest.setFree(false);
+		oktoberfest.setWeekday("fre");
+		oktoberfest.setDayNumber("29");
+		oktoberfest.setPrice(300);
+		oktoberfest.setDescription("Den 10. oktober 2011 kl. 13.00 braker det løs i Dødens Dal, når feststemte mennesker iført lederhosen samles rundt langbordene for et ølgilde man ellers må helt til Tyskland for å få oppleve. Ølglass på høykant, allsang, dans og generelt god stemning står på programmet for årets Oktoberfest. Det blir også livemusikk av både DJs og band i teltet. Det er ikke tilfeldig at dette er eventen som i følge historiebøkene blir raskest utsolgt. Tradisjonen kommer opprinnelig fra delstaten Bayern i Tyskland, men har med årene også blitt populær i lille Norge. Har man ikke muligheten til å dra sørover for å oppleve det på ekte tysk vis, kan man like så godt dra på seg lederhosen og slenge ølmagen på bardisken her i Trondheim. UKA har arrangert Oktoberfester siden 2001, og festen har med årene blitt en UKEtradisjon. Trondheims befolkning både forbinder og forventer denne festen av oss, og i år tar vi den til helt nye høyder. Finn frem kostymet og forbered deg til årets fest. Billettene kommer med ”god stemnings”-garanti. Dette vil du ikke gå glipp av!");
+		eventsArrayList.add(oktoberfest);
+
+		Event kaizers = new Event();
+		kaizers.setTitle("Kaizers Orchestra");
+		kaizers.setPlace("Storsalen");	
+		kaizers.setEventType("Konsert");
+		kaizers.setAgeLimit(18);
+		kaizers.setStartTime("19:00");
+		kaizers.setWeekday("lør");
+		kaizers.setDayNumber("30");
+		kaizers.setPrice(350);
+		kaizers.setDescription("Den 20. oktober 2011 kl. 13.00 braker det løs i Dødens Dal, når feststemte mennesker iført lederhosen samles rundt langbordene for et ølgilde man ellers må helt til Tyskland for å få oppleve. Ølglass på høykant, allsang, dans og generelt god stemning står på programmet for årets Oktoberfest. Det blir også livemusikk av både DJs og band i teltet. Det er ikke tilfeldig at dette er eventen som i følge historiebøkene blir raskest utsolgt. Tradisjonen kommer opprinnelig fra delstaten Bayern i Tyskland, men har med årene også blitt populær i lille Norge. Har man ikke muligheten til å dra sørover for å oppleve det på ekte tysk vis, kan man like så godt dra på seg lederhosen og slenge ølmagen på bardisken her i Trondheim. UKA har arrangert Oktoberfester siden 2001, og festen har med årene blitt en UKEtradisjon. Trondheims befolkning både forbinder og forventer denne festen av oss, og i år tar vi den til helt nye høyder. Finn frem kostymet og forbered deg til årets fest. Billettene kommer med ”god stemnings”-garanti. Dette vil du ikke gå glipp av!");
+
+		kaizers.setFree(false);
+
+		eventsArrayList.add(kaizers);
+
+
+		eventsArrayList.add(oktoberfest);
+		eventsArrayList.add(kaizers);
+
+		eventListView.setOnItemClickListener(new OnItemClickListener(){
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+				//viewContact.setClass(getApplicationContext(), ContactDetailsActivity.class);
+				/**
+					databaseHandler.open();
+					databaseHandler.removePerson(contactArrayList.get(position).getId());
+					databaseHandler.close();
+					populateList();
+				 */
+				Intent viewEvent = new Intent();
+				viewEvent.setClass(getApplicationContext(), EventDetailsActivity.class);
+				viewEvent.putExtra("SelectedEvent", eventsArrayList.get(position));///////
+				startActivity(viewEvent); 
+			}
+		});
+
+	} 
+
+}
+
