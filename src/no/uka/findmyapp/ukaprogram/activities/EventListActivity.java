c3a53c9e2c95f9dd643952e780c41a9baeea984b
Torstein Melhus Barkve <torstein@barkve.no>
2011-07-20 18:15:31 +0200

    minor changes

--- src/no/uka/findmyapp/ukaprogram/activities/EventListActivity.java
+++ src/no/uka/findmyapp/ukaprogram/activities/EventListActivity.java
@@ -10,8 +10,6 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.ListView;
