28caa7521ff8e3e6b0a3c3ecd402a4445fe33a18
Torstein Melhus Barkve <torstein@barkve.no>
2011-07-20 19:52:47 +0200

    added startuploader

---
+++ src/no/uka/findmyapp/ukaprogram/activities/StartupLoader.java
@@ -0,0 +1,27 @@
+package no.uka.findmyapp.ukaprogram.activities;
+
+import no.uka.findmyapp.android.rest.client.RestServiceHelper;
+import no.uka.findmyapp.ukaprogram.R;
+import no.uka.findmyapp.ukaprogram.utils.EventsUpdater;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class StartupLoader extends Activity {
+	private static final String debug = "StartupLoader";
+
+	private static RestServiceHelper serviceHelper = RestServiceHelper.getInstance(); 
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.splash);
+		
+		EventsUpdater eu = new EventsUpdater(getApplicationContext());
+		eu.initUpdate(); 
+	}
+}
