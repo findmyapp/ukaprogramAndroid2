1351ab53b7c71202860dfb8b7275e38741be77e2
Ole Christian Roed <ocroed@gmail.com>
2011-07-21 21:10:09 +0200

    Starting some new shit

---
+++ src/no/uka/findmyapp/ukaprogram/activities/PlacesActivity.java
@@ -0,0 +1,15 @@
+package no.uka.findmyapp.ukaprogram.activities;
+
+import no.uka.findmyapp.ukaprogram.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlacesActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.places);
+	}
+
+}
