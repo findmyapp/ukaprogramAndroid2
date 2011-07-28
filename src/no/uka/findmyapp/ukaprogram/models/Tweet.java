ad1f0a6c46add3feec950451f8d02cb074cf621e
Jonas Korssjoen <jonasgutvik@gmail.com>
2011-07-28 22:35:04 +0200

    Fikset caching av twitter stasj. Prøvd å fikse riktig tweet til UKA, ikke fått til.

--- src/no/uka/findmyapp/ukaprogram/models/Tweet.java
+++ src/no/uka/findmyapp/ukaprogram/models/Tweet.java
@@ -1,9 +1,15 @@
 package no.uka.findmyapp.ukaprogram.models;
 
+import android.graphics.Bitmap;
+
 public class Tweet {
 	public String username;
 	public String message;
 	public String image_url;
+	public Boolean usernameSet = false;
+	public Boolean messageSet = false;
+	public Boolean imageSet = false;
+	public Bitmap avatar;
 
 	public Tweet(String username, String message, String url) {
 		this.username = username;
